c3f9297fad97e766135407ccfe7c366415618ed3
HYUNAHSHIM <hyunahshim@naver.com>
2022-01-12 20:30:05 +0900

    📰Feat: Add PortfolioRepository file

---
+++ src/main/java/HYLikeLion/gitppo/gitppoProject/repository/Portfolio/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package HYLikeLion.gitppo.gitppoProject.repository.Portfolio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import HYLikeLion.gitppo.gitppoProject.domain.portfolio.Portfolio;
+
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+
+}
