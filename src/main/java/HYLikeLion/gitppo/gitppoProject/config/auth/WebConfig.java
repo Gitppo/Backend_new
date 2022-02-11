e030d619f27cf888f0e7b3d34dec368ab93aa9f9
HYUNAHSHIM <hyunahshim@naver.com>
2022-02-11 22:53:03 +0900

    🚑Fix: CORS to localhost:3000

--- src/main/java/HYLikeLion/gitppo/gitppoProject/config/auth/WebConfig.java
+++ src/main/java/HYLikeLion/gitppo/gitppoProject/config/auth/WebConfig.java
@@ -22,6 +22,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*");
+			.allowedOrigins("http://localhost:3000");
 	}
 }
